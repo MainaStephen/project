2a02c72a6cf745e0e4971f20c44b819fdacfbc8d
MainaStephen <mainastephen159@gmail.com>
2023-05-15 16:56:34 +0300

    nw

---
+++ function.js
@@ -0,0 +1,21 @@
+function greet(fname,lname){
+    alert("good evening",fname,lname)
+}
+
+//calling function
+greet("steve","Gachoka");
+// //console.log("good evening" + fname +''+ lname)
+
+
+// function sum (num1,num2){
+//     let added =num1 + num2
+//     console.log("result is",added)
+// }
+
+// sum( 32,10)
+
+// function returnsums(num1,num2){
+//     return num1 + num2
+// }
+// const addednums = returnsums(10,20)
+// console.log("result is", addednums)
---
+++ new.js
@@ -0,0 +1,22 @@
+// console.log("this is my first js class")
+
+// person = {
+//     age: 45,
+//     gender: "male",
+//     name: "john_doe",
+//     weight: 22,
+// };
+// console.log("the age is",person.age,"years old")
+
+
+let mark =prompt("enter your marks:");
+
+if (mark > 80){
+    console.log("grade = A")
+}else if(mark >= 70 && mark <= 79){
+    console.log("Grade = B")
+}else if(mark>=60  && mark <= 69){
+    console.log("Grade = C")   
+}else{
+    console.log("grade = D")
+}
